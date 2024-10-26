55ce3d732207712a816e3478dd3878a40e10411b
Elmagn1f1co <steveamadi007@gmail.com>
2024-10-26 02:13:57 +0100

    Project

---
+++ MagicVilla_VillaAPI/Migrations/20231110045017_NewPublicId.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewPublicId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PublicId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PublicId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "PublicId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "PublicId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "PublicId",
+                value: null);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Villas");
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/ImageService.cs
@@ -0,0 +1,43 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace MagicVilla_VillaAPI.Services
+{
+	public class ImageService
+	{
+		private readonly Cloudinary _cloudinary;
+		public ImageService(IConfiguration config)
+		{
+			var acc = new Account
+			(
+				config["Cloudinary:CloudName"],
+				config["Cloudinary:ApiKey"],
+				config["Cloudinary:ApiSecret"]
+			);
+			_cloudinary = new Cloudinary(acc);
+		}
+		public async Task<ImageUploadResult> AddImageAsync(IFormFile file)
+		{
+			var uploadresult = new ImageUploadResult();
+
+			if (file.Length > 0)
+			{
+				using var stream = file.OpenReadStream();
+				var uploadParams = new ImageUploadParams
+				{
+					File = new FileDescription(file.FileName, stream)
+				};
+				uploadresult = await _cloudinary.UploadAsync(uploadParams);
+			}
+
+			return uploadresult;
+		}
+
+		public async Task<DeletionResult> DeleteImageAsync(string publicId)
+		{
+			var deleteParams = new DeletionParams(publicId);
+			var result = await _cloudinary.DestroyAsync(deleteParams);
+			return result;
+		}
+	}
+}
